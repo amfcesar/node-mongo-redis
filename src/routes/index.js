aa4fab3a46d1b3db98bfa24985aee26b3621647d
Cesar <amfcesar@gmail.com>
2020-07-15 10:55:38 -0300

    change route health.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { description, version } = require('../../package.json');
 const router = express.Router();
 
-router.get('/',  (req, res, next) => {
+router.get('/health',  (req, res, next) => {
     res.status(200).send({ 
         title: description,
         version
--- src/routes/personRoute.js
+++ src/routes/personRoute.js
@@ -5,7 +5,6 @@ const { userPost } = require('../schema/schema')
 const controller = require('../controllers/personController');
 const middleware = require('../middleware/middlewareValidation'); 
 
-
 router.get('/', controller.get);
 router.get('/:id', controller.getById);
 router.post('/', middleware(userPost), controller.post);
